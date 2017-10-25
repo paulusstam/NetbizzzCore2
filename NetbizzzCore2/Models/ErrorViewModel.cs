4aa12c5339f96481fc2c050e4a38fad44c285a09
Pavlos Stam <pstamat@outlook.com>
2017-10-25 03:31:08 +0300

    Add project files.

---
+++ NetbizzzCore2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetbizzzCore2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
